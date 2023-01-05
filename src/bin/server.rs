8407f9d5cf03fc7fe3775cc28ef5f62af5cb1e99
Nicholas Sandomeno <nicholassandomeno@Nicholass-MBP.fios-router.home>
2023-01-05 10:28:47 -0500

    through async in depth chapter of tutorial

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,38 +1,36 @@
 use bytes::Bytes;
-use std::sync::{Arc, Mutex};
+use mini_redis::{Connection, Frame};
 use std::collections::HashMap;
-use tokio::sync::{mpsc, oneshot};
-use tokio::net::{TcpStream, TcpListener};
-use mini_redis::{Connection, Frame, client};
+use std::sync::{Arc, Mutex};
+use tokio::net::{TcpListener, TcpStream};
 
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
-type ShardedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
-type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
-#[derive(Debug)]
-enum Command {
-    Get {
-        key: String,
-        resp: Responder<Option<Bytes>>,
-    },
-    Set {
-        key: String,
-        val: Bytes,
-        resp: Responder<Option<Bytes>>,
-    }
-}
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-fn new_sharded_db(num_shards: usize) -> ShardedDb {
-    let mut db = Vec::with_capacity(num_shards);
-    for _ in 0..num_shards {
-        db.push(Mutex::new(HashMap::new()));
+    println!("Listening");
+
+    // A hashmap is used to store data
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        // Clone the handle to the hash map.
+        let db = db.clone();
+
+        println!("Accepted");
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
     }
-    Arc::new(db)
 }
 
 async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    // The Connection from mini-redis handles parsing frames from
+
+    // Connection, provided by `mini-redis`, handles parsing frames from
     // the socket
     let mut connection = Connection::new(socket);
 
@@ -46,9 +44,6 @@ async fn process(socket: TcpStream, db: Db) {
             Get(cmd) => {
                 let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    // Frame::Bulk expects data to be of type Bytes. This
-                    // type will be covered later. For now &Vec<u8> is converted to Bytes
-                    // using into()
                     Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
@@ -57,29 +52,7 @@ async fn process(socket: TcpStream, db: Db) {
             cmd => panic!("unimplemented {:?}", cmd),
         };
 
-        // Writes the response to the client
+        // Write the response to the client
         connection.write_frame(&response).await.unwrap();
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    println!("Listening");
-
-    let db: Db = Arc::new(Mutex::new(HashMap::new()));
-
-    loop {
-        // the second item contains the IP and port of the new connection.
-        let (socket, _) = listener.accept().await.unwrap();
-        // Clone the DB
-        let db = db.clone();
-        // A new task is spawned for each inbound socket. The socket is 
-        // moved to the new task and processed there.
-
-        println!("Accepted");
-        tokio::spawn(async move {
-            process(socket, db).await;
-        });
     }
 }
